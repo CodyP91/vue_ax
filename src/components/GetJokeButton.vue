fcf50e724f6d0c76b761c2c5c36b0d2627e92d4f
Cody <cjoel91@hotmail.com>
2023-05-13 06:25:50 -0600

    Updates

---
+++ src/components/GetJokeButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="getJoke">Get New Joke</button>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'GetJokeButton',
+    methods: {
+      getJoke() {
+        axios
+          .get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+          .then(response => {
+            this.$emit('new-joke', response.data[0]);
+          })
+          .catch(error => {
+            console.error('Error fetching joke:', error);
+          });
+      },
+    },
+  };
+  </script>
+  
